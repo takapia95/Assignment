b2865bd0b2cb3aeeff940c58d75630653e2aa449
Ai Tran <113641497+takapia95@users.noreply.github.com>
2022-10-21 05:50:10 +0000

    fix form

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 window.onload = init;
 function init() {
-  addLastUpdate();
+  draw();
 }
- function validationForm() {
-	      var name=['Harry','Elizabeth','Shana'];
-	      var password=document.getElementById("pass").value;
-		  var repass=document.getElementById("second_pass").value;
-		  var user=document.getElementById("username").value;
-		  if(password.length<8)
-		  {
-		     document.getElementById("error").innerHTML="Password length must be greater than 8 characters";
-		  }
-		   else if(password!==repass)
-		   {
-		    document.getElementById("error").innerHTML="Password does not match";
-          
-		   }
-		   else
-		   {
-		      document.getElementById("error").innerHTML="";
-		   }
-		   for(var i=0;i<name.length;i++)
-		   {
-		      if(user==name[i])
-			  {
-			     document.getElementById("error").innerHTML="User Already Existed";
-			  }
-		   }
+//function to validate form on page 3
+//onsubmit on form
+function validationForm() {
+  return validPass() && validUser();
+}
+//validates pass and repass are matching on page 3
+function validPass() {
+  var pass1 = document.getElementById('pass').value;
+  var pass2 = document.getElementById('second_pass').value;
+  if (pass1 !== pass2) {
+    alert("Passwords need to match");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+//make sure the name is not already used on page 3
+function validUser() {
+  var users = ["Harry", "Elizabeth", "Shana"];
+  var name = document.forms["reg_form"]["username"].value;
+  if (users.includes(name)) {
+    alert("Username already used");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function draw() {
+  var canvas = document.getElementById("myCanvas");
+  if (canvas) {
+    if (canvas.getContext) {
+      var ctx = canvas.getContext("2d");
+      ctx.font = "30px Comic Sans MS";
+      ctx.fillStyle = "red";
+      ctx.textAlign = "center";
+      ctx.fillText("Hello World", 150, 100);
+    } else {
+      alert('You need Safari or Firefox 1.5+ to see this demo.');
     }
+  }
+}
